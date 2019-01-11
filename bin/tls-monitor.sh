997de46888e56c80eb498d2704f7d733e6f81522
Jos Ahrens <buughost@gmail.com>
2019-01-11 16:25:58 +0000

    shellcheck lint fixes

--- bin/tls-monitor.sh
+++ bin/tls-monitor.sh
@@ -2,14 +2,14 @@
 
 TemporaryFileStorage="./cert_will_expire.txt"
 
-if [[ "x$2" == "x" || "x$1" == "x" ]]; then
+if [[ -z $1 || -z $2 ]]; then
     echo "Usage: $0 <website> <expiry_in_days> [port]"
     exit 1
 fi
 
-ReminderWhenDaysLeft=$2
-ServerToCheck=$1
-if [[ "x$3" == "x" ]]; then
+ReminderWhenDaysLeft="$2"
+ServerToCheck="$1"
+if [[ -z $3 ]]; then
     PortToCheck=443
 else
     PortToCheck=$3
@@ -17,22 +17,21 @@ fi
 ExitCode=0
 ProgramDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-openssl s_client -connect ${ServerToCheck}:${PortToCheck} -servername ${ServerToCheck} <<< 'Q' 2>/dev/null \
+openssl s_client -connect "$ServerToCheck":"$PortToCheck" -servername "$ServerToCheck" <<< 'Q' 2>/dev/null \
     | openssl x509 -noout -dates > ${TemporaryFileStorage} \
     | true # always succeed
 
-ExpireInfo=$(cat ${TemporaryFileStorage} | tail -1)
-ExpiryDate=`date --date="$(echo $ExpireInfo | grep -o -P '(?<==).*')" --iso-8601`
+ExpireInfo=$(tail -1 < $TemporaryFileStorage)
+ExpiryDate=$(date --date="$(echo "$ExpireInfo" | grep -o -P '(?<==).*')" --iso-8601)
 
-$ProgramDirectory/date-check.py "$ExpiryDate" "$ReminderWhenDaysLeft" "$PortToCheck"
+"$ProgramDirectory"/date-check.py "$ExpiryDate" "$ReminderWhenDaysLeft" "$PortToCheck"
 CheckDateExitCode=$?
 
-if [[ $CheckDateExitCode -eq 1 ]]; then
-    ExitCode=$(($ExitCode + 1))
-    echo "Close to expiry: $url"
+if [[ "$CheckDateExitCode" -eq 1 ]]; then
+    ExitCode=$((ExitCode + 1))
     cat ${TemporaryFileStorage}
     echo
 fi
 
-rm ${TemporaryFileStorage}
-exit $ExitCode
+rm "$TemporaryFileStorage"
+exit "$ExitCode"
